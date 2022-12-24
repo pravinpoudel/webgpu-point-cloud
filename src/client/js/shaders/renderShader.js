30fe8459c88d192b4cd9e1ff0645fa305125c7b2
pravinpoudel <prvnpoudel4@gmail.com>
2022-12-24 15:03:58 -0700

    shader code u32 into f32 not working

--- src/client/js/shaders/renderShader.js
+++ src/client/js/shaders/renderShader.js
@@ -11,18 +11,20 @@ let vs = `
 
     @group(0) @binding(0) var<uniform> MVP_Matrix: mat4x4<f32>;
 
-    fn unpacku32(color:vec4<uint32>)->vec3<f32>{
-        let rgba = vec3<f32>(0, 0, 0, 0, 1.0);
-        rgba.x = ((color.x))
-        rbga.y =
-        rgba.y = 
-   
+    fn unpacku32(color:vec4<u32>)->vec3<f32>{
+
+        let rgba = vec4<f32>(0.0, 0.0, 0.0, 1.0);
+        let color1:vec4<f32> = vec4<f32>(color);
+        rgba.x = (color1.x/255.0);
+        rgba.y = (color1.y/255.0);
+        rgba.z = (color1.z/255.0);
+        return clamp(rgba, vec3<f32>(0.0), vec3<f32>(1.0));
     }
 
     @vertex
     fn main(in: VertexInput)->VertexOut{
         var out:VertexOut;
-        out.color = in.color;
+        out.color = unpacku32(in.color);
         out.position = MVP_Matrix* vec4<f32>(in.position, 1.0);
         return out;
     }
